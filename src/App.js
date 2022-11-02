36999015c04457f8e2850b54e8ec7f9a4c3e463b
Adhitya Noor Muslim <adhitya.muslim@prudential.co.id>
2022-11-02 14:04:51 +0700

    complete update and delete functions

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import Form from "react-bootstrap/Form";
 import ListGroup from 'react-bootstrap/ListGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import { addItem, deleteItem } from "./redux/modules/todos";
+import { addItem, deleteItem, updateItem } from "./redux/modules/todos";
+import Modal from 'react-bootstrap/Modal';
 
 import { v4 as uuid } from 'uuid';
 
@@ -40,7 +41,7 @@ const TodoForm = () => {
     const newId = uuid().slice(0, 8);
     setTodo(current => ({ ...current, id: newId }))
 
-    if (!(toDo.title && toDo.description)) return; //do nothing if toDo falsified
+    if (!(toDo.title && toDo.description)) return; //do nothing if toDo properties falsified
     dispatch(addItem(toDo));
     alert(toDo.title + ' ' + toDo.description + ' ' + toDo.id + ' ' + toDo.isDone);
   };
@@ -69,28 +70,64 @@ const TodoForm = () => {
   );
 };
 
+
 const TodoList = () => {
 
   const dispatch = useDispatch();
 
-
+  const [modalShow, setModalShow] = useState(false);
 
   const toDos = useSelector(state => state.toDos.items);
-  const count = useSelector(state => state.toDos.items.length);
+  // const count = useSelector(state => state.toDos.items.length);
+
+  const toDosIncomplete = toDos.filter(incompleted => { return !incompleted.isDone });
+  const countIncomplete = toDosIncomplete.length;
+
+  const toDosComplete = toDos.filter(completed => { return completed.isDone });
+  const countComplete = toDosComplete.length;
 
   const deleteTodo = (taskId) => {
-    alert(taskId);
     dispatch(deleteItem(taskId));
   };
+
+  const updateTodo = (taskId, isDone) => {
+    const payload = {
+      id: taskId,
+      status: isDone ? false : true
+    }
+    alert(taskId + ' is going to be updated ' + payload.status);
+    dispatch(updateItem(payload));
+  };
+
+  const DeleteConfirmationModal = (props) => {
+    return (
+      <Modal
+        {...props}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Delete task ?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button variant="outline-primary" onClick={() => { deleteTodo(props.taskid) }}>Yes</Button>
+          <Button onClick={props.onHide}>No</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
 
 
   // const toDos = ['Task 1','Task 2','Task 3'];
   return (
     <Container>
-      <h5>To Dos : {count}</h5>
+      <h5>To Dos : {countIncomplete}</h5>
       <ListGroup variant="flush" as="ol" numbered>
-        {toDos.map(task => {
-
+        {toDosIncomplete.map(task => {
           return (<ListGroup.Item
             as="li"
             className="d-flex justify-content-between align-items-start"
@@ -106,16 +143,49 @@ const TodoList = () => {
               title={'Action'}
               size=""
             >
-              <Dropdown.Item >Complete task</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item onClick={() => { deleteTodo(task.id) }} >Delete task</Dropdown.Item>
-            </DropdownButton>
-          </ListGroup.Item>)
+              <Dropdown.Item onClick={() => { updateTodo(task.id, task.isDone) }} >Complete task</Dropdown.Item>
 
+              <Dropdown.Item onClick={() => setModalShow(true)} >Delete task</Dropdown.Item>
+              <DeleteConfirmationModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                taskid={task.id}
+              />
+            </DropdownButton>
+          </ListGroup.Item>
+          )
         })}
       </ListGroup>
-      <h5>Completed:</h5>
+      <h5>Completed: {countComplete}</h5>
+      <ListGroup variant="flush" as="ol" numbered>
+        {toDosComplete.map(task => {
+          return (<ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start"
+            key={task.id}
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold"><s>{task.title}</s></div>
+              <s>{task.description}</s>
+            </div>
+            <DropdownButton
+              drop={'end'}
+              variant="link"
+              title={'Action'}
+              size=""
+            >
+              <Dropdown.Item onClick={() => { updateTodo(task.id, task.isDone) }} >Un-Complete task</Dropdown.Item>
 
+              <Dropdown.Item onClick={() => setModalShow(true)} >Delete task</Dropdown.Item>
+              <DeleteConfirmationModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                taskid={task.id}
+              />
+            </DropdownButton>
+          </ListGroup.Item>)
+        })}
+      </ListGroup>
 
     </Container>
   );
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,21 +1,19 @@
 // const init = {
-//     items: [{id: 0,title: '',description: ''}],
+//     items: [{id: 0,title: '',description: '',isDone : true}],
 // };
 
 const init = {
     items: []
 };
 
-//actions
+//action creators using Duck pattern
 const ADD_ITEM = 'todo-list/todos/ADD_ITEM';
-
 const DELETE_ITEM = 'todo-list/todos/DELETE_ITEM';
-
-// const UPDATE_ITEM = 'todo-list/todos/UPDATE_ITEM';
+const UPDATE_ITEM = 'todo-list/todos/UPDATE_ITEM';
 
 export const addItem = toDoItem => ({type: ADD_ITEM, payload: toDoItem});
-
 export const deleteItem = toDoItem => ({type: DELETE_ITEM, payload: toDoItem});
+export const updateItem = toDoItem => ({type: UPDATE_ITEM, payload: toDoItem});
 
 const todoReducer = (state=init, action) => {
     switch (action.type) {
@@ -26,6 +24,10 @@ const todoReducer = (state=init, action) => {
         case DELETE_ITEM:
             return {
                 items: [...state.items.filter(item => item.id !== action.payload)]
+            };
+        case UPDATE_ITEM:
+            return {
+                items: [...state.items.map(item => item.id === action.payload.id ? {...item, isDone : action.payload.status} : item)]
             };
         default:
             return state;
